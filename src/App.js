41e3906381d16e78ef5943cdd799a1702184cb6b
S Shraddha <shraddha.jain3219@gmail.com>
2021-03-06 01:10:40 +0530

    refactor: fetch inventory list object from Utility dir

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import "./styles.css";
 import { useState, useEffect } from "react";
 import heroIcon from "../public/shopping-cart.svg";
+import { inventoryList } from "./Utility/constants.js";
 
 export default function App() {
-  const [inventory, setInventory] = useState({
-    ITEM01: {
-      name: "Moong Dal Yellow (Split)",
-      img:
-        "https://rukminim1.flixcart.com/image/352/352/j8rnpu80/pulses/m/j/c/1-moong-dal-moong-dal-un-branded-original-imaeymjgdha3wqts.jpeg?q=70",
-      quantity: 5,
-      price: 128,
-      unit: "kg",
-      perUnit: 1,
-      inStock: true
-    },
-    ITEM02: {
-      name: "Red Bull Sugar free Energy Drink",
-      img:
-        "https://rukminim1.flixcart.com/image/352/352/kf2v3ww0/energy-sport-drink-mix/g/y/z/6000-sugarfree-red-bull-original-imafvmeft3jhruky.jpeg?q=70",
-      quantity: 5,
-      price: 115,
-      unit: "ml",
-      perUnit: 250,
-      inStock: true
-    },
-    ITEM03: {
-      name: "Britannia Treat Jim Jam",
-      img:
-        "https://rukminim1.flixcart.com/image/352/352/k8j3gcw0/cookie-biscuit/e/t/g/150-treat-jim-jam-britannia-original-imafqgw8mhyxgmy3.jpeg?q=70",
-      quantity: 3,
-      price: 31,
-      unit: "g",
-      perUnit: 250,
-      inStock: true
-    }
-  });
+  const [inventory, setInventory] = useState(inventoryList);
 
   const [amount, setAmount] = useState(0);
   const [cart, setCart] = useState({});
---
+++ src/Utility/constants.js
@@ -0,0 +1,34 @@
+export { inventoryList };
+
+const inventoryList = {
+  ITEM01: {
+    name: "Moong Dal Yellow (Split)",
+    img:
+      "https://rukminim1.flixcart.com/image/352/352/j8rnpu80/pulses/m/j/c/1-moong-dal-moong-dal-un-branded-original-imaeymjgdha3wqts.jpeg?q=70",
+    quantity: 5,
+    price: 128,
+    unit: "kg",
+    perUnit: 1,
+    inStock: true
+  },
+  ITEM02: {
+    name: "Red Bull Sugar free Energy Drink",
+    img:
+      "https://rukminim1.flixcart.com/image/352/352/kf2v3ww0/energy-sport-drink-mix/g/y/z/6000-sugarfree-red-bull-original-imafvmeft3jhruky.jpeg?q=70",
+    quantity: 5,
+    price: 115,
+    unit: "ml",
+    perUnit: 250,
+    inStock: true
+  },
+  ITEM03: {
+    name: "Britannia Treat Jim Jam",
+    img:
+      "https://rukminim1.flixcart.com/image/352/352/k8j3gcw0/cookie-biscuit/e/t/g/150-treat-jim-jam-britannia-original-imafqgw8mhyxgmy3.jpeg?q=70",
+    quantity: 3,
+    price: 31,
+    unit: "g",
+    perUnit: 250,
+    inStock: true
+  }
+};
